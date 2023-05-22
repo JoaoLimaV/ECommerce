f007636726ce526c329140bfe51eea5bf5838a39
JoaoLimaV <joaolima675@outlook.com>
2023-05-21 22:06:43 -0300

    Structural ECommerce

---
+++ src/ProductDao.java
@@ -0,0 +1,16 @@
+import java.util.List;
+
+public class ProductDao {
+	
+	public String update(Product product) {
+		return "Sucessfull update";
+	}
+	
+	public Product find(int id) {
+		return null;
+	}
+
+	public List<Product> findAll() {
+		return null;
+	}
+}
